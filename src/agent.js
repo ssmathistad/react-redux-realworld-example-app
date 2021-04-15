b189b2b2f78f34e28c55ce662b594dc73aedb27e
Samantha Mathistad <ssmathistad@mail.csuchico.edu>
2021-04-15 07:45:40 -0700

    Update url to point to backend

--- src/agent.js
+++ src/agent.js
@@ -3,7 +3,8 @@ import _superagent from 'superagent';
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
-const API_ROOT = 'https://conduit.productionready.io/api';
+//const API_ROOT = 'https://conduit.productionready.io/api';
+const API_ROOT = 'http://host.docker.internal:3000';
 
 const encode = encodeURIComponent;
 const responseBody = res => res.body;
